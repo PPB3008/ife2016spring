53041bf8e573fb2fc4c8739e88cc00151f68f1cd
hunter <280319836@qq.com>
2016-03-31 16:46:14 +0800

    修改代码，属性与方法分离，创建飞船不丢包

--- stage2/js/task26.js
+++ stage2/js/task26.js
@@ -8,13 +8,14 @@ var mediator = {
     },
     // 接收传来的信息
     subscribe: function (signal) {
-        var _self = this;
+        var self = this;
 
         setTimeout(function () {
-            var ships = _self.ships, ship;
+            var ships = self.ships,
+                ship;
 
             console.log(signal.command + " command publishing");
-            _self.publish(signal);
+            self.publish(signal);
 
             // 接收到删除命令则将飞船从飞船队列删除,这个语句必须要在publish之后进行。因为Ship的publish需要访问飞船队列
             if(signal.command == "destroy") {
@@ -25,23 +26,13 @@ var mediator = {
     // 广播命令到飞船队列中所有飞船
     publish: function (signal) {
         // 丢包率30%
-        var randomNum = Math.floor(Math.random() * 100),
+        var randomNum       = Math.floor(Math.random() * 100),
             loseProbability = 30;
 
         if(randomNum >= loseProbability) {
-            if(signal.command == "create") {
-                for(var i = 0; i < 4; i++) {
-                    if(!this.ships[i]) {
-                        ship = new Ship(i+1);
-                        this.addShip(ship, i);
-                        break;
-                    }
-                }
-            } else {
-                for(var iter in this.ships) {
-                    if(this.ships[iter] && this.ships[iter] instanceof Ship) {
-                        this.ships[iter].subscribe(signal);
-                    }
+            for(var iter in this.ships) {
+                if(this.ships[iter] && this.ships[iter] instanceof Ship) {
+                    this.ships[iter].subscribe(signal);
                 }
             }
             console.log(signal.command + " command done");
@@ -74,140 +65,152 @@ var Ship = function (index) {
     this.controlBar;
     // 时间间隔
     this.interval;
-    // 创建自身
-    this.createSelf = function () {
-        var earth      = document.querySelector(".earth"),
-            cmd        = document.querySelector(".cmd"),
-            ship       = document.createElement("div"),
-            controlBar = document.createElement("div"),
-            number     = document.createElement("span"),
-            startBtn   = document.createElement("button"),
-            stopBtn    = document.createElement("button"),
-            destroyBtn = document.createElement("button");
 
-        ship.className       = "ship" + this.index;
-        ship.innerHTML       = parseInt(this.power/10, 10) + "%";
-        number.innerHTML     = "对" + this.index + "号进行操作: ";
-        startBtn.innerHTML   = "开始飞行";
-        stopBtn.innerHTML    = "停止飞行";
-        destroyBtn.innerHTML = "销毁";
-
-        controlBar.appendChild(number);
-        controlBar.appendChild(startBtn);
-        controlBar.appendChild(stopBtn);
-        controlBar.appendChild(destroyBtn);
-        earth.appendChild(ship);
-        cmd.appendChild(controlBar);
-
-        this.ship = ship;
-        this.controlBar = controlBar;
-    };
-    // 动力系统
-    this.fly = function () {
-        if(1 == this.state) {
-            return false;
-        }
-        this.state = 1;
-
-        var _self = this;
-
-        clearInterval(this.interval);
-        this.interval = setInterval(function () {
-            if(_self.power < 5) {
-                _self.stop();
-                return ;
-            }
-
-            _self.power += (_self.consume + _self.restore);
-            _self.ship.innerHTML = parseInt(_self.power/10, 10) + "%";
-            _self.rotate += _self.speed;
-
-            if(_self.rotate >= 360) {
-                _self.rotate = 0;
-            }
-
-            _self.ship.style.webkitTransform = "rotate("+_self.rotate+"deg)";
-            _self.ship.style.mozTransform    = "rotate("+_self.rotate+"deg)";
-            _self.ship.style.msTransform     = "rotate("+_self.rotate+"deg)";
-            _self.ship.style.oTransform      = "rotate("+_self.rotate+"deg)";
-            _self.ship.style.transform       = "rotate("+_self.rotate+"deg)";
-        }, 80);
-    };
-    this.stop = function () {
-        if(0 == this.state) {
-            return false;
-        }
-        this.state = 0;
-
-        var _self = this;
-
-        clearInterval(this.interval);
-        this.interval = setInterval(function () {
-            _self.power += _self.restore;
-            _self.ship.innerHTML = parseInt(_self.power/10, 10) + "%";
-            if(_self.power >= 1000) {
-                _self.power = 1000;
-                clearInterval(_self.interval);
-                return ;
-            }
-        }, 80);
-    };
-    // 自毁系统
-    this.destroySelf = function () {
-        var earth = document.querySelector(".earth");
-
-        this.stop();
-        earth.removeChild(this.ship);
-    };
-    // 信号接收与处理系统
-    this.subscribe = function (signal) {
-        if(signal.index != this.index) {
-            return false;
-        }
-        var command = signal.command;
-        switch (command) {
-            case "fly":
-                this.fly();
-                break;
-            case "stop":
-                this.stop();
-                break;
-            case "destroy":
-                this.destroySelf();
-                break;
-            default:
-                console.log("command rejected");
-                break;
-        }
-    };
-
+    // 构造函数执行，创建自身
     this.createSelf();
 };
 
+// 创建自身
+Ship.prototype.createSelf = function () {
+    var earth      = document.querySelector(".earth"),
+        cmd        = document.querySelector(".cmd"),
+        ship       = document.createElement("div"),
+        controlBar = document.createElement("div"),
+        number     = document.createElement("span"),
+        startBtn   = document.createElement("button"),
+        stopBtn    = document.createElement("button"),
+        destroyBtn = document.createElement("button");
+
+    ship.className       = "ship" + this.index;
+    ship.innerHTML       = parseInt(this.power/10, 10) + "%";
+    number.innerHTML     = "对" + this.index + "号进行操作: ";
+    startBtn.innerHTML   = "开始飞行";
+    stopBtn.innerHTML    = "停止飞行";
+    destroyBtn.innerHTML = "销毁";
+
+    controlBar.appendChild(number);
+    controlBar.appendChild(startBtn);
+    controlBar.appendChild(stopBtn);
+    controlBar.appendChild(destroyBtn);
+    earth.appendChild(ship);
+    cmd.appendChild(controlBar);
+
+    this.ship = ship;
+    this.controlBar = controlBar;
+};
+
+// 动力系统
+Ship.prototype.fly = function () {
+    if(1 == this.state) {
+        return false;
+    }
+    this.state = 1;
+
+    var self = this;
+
+    clearInterval(this.interval);
+    this.interval = setInterval(function () {
+        if(self.power < 5) {
+            self.stop();
+            return ;
+        }
+
+        self.power += (self.consume + self.restore);
+        self.ship.innerHTML = parseInt(self.power/10, 10) + "%";
+        self.rotate += self.speed;
+
+        if(self.rotate >= 360) {
+            self.rotate = 0;
+        }
+
+        self.ship.style.webkitTransform = "rotate("+self.rotate+"deg)";
+        self.ship.style.mozTransform    = "rotate("+self.rotate+"deg)";
+        self.ship.style.msTransform     = "rotate("+self.rotate+"deg)";
+        self.ship.style.oTransform      = "rotate("+self.rotate+"deg)";
+        self.ship.style.transform       = "rotate("+self.rotate+"deg)";
+    }, 80);
+};
+
+Ship.prototype.stop = function () {
+    if(0 == this.state) {
+        return false;
+    }
+    this.state = 0;
+
+    var self = this;
+
+    clearInterval(this.interval);
+    this.interval = setInterval(function () {
+        self.power += self.restore;
+        self.ship.innerHTML = parseInt(self.power/10, 10) + "%";
+        if(self.power >= 1000) {
+            self.power = 1000;
+            clearInterval(self.interval);
+            return ;
+        }
+    }, 80);
+};
+
+// 自毁系统
+Ship.prototype.destroySelf = function () {
+    var earth = document.querySelector(".earth");
+
+    this.stop();
+    earth.removeChild(this.ship);
+};
+
+// 信号接收与处理系统
+Ship.prototype.subscribe = function (signal) {
+    if(signal.index != this.index) {
+        return false;
+    }
+    var command = signal.command;
+    switch (command) {
+        case "fly":
+            this.fly();
+            break;
+        case "stop":
+            this.stop();
+            break;
+        case "destroy":
+            this.destroySelf();
+            break;
+        default:
+            console.log("command rejected");
+            break;
+    }
+};
+
 /* 指挥官 */
-var Commander = function () {
-    this.command = function (signal) {
-        mediator.subscribe(signal);
-    };
+var Commander = function () {};
+
+// 发布命令
+Commander.prototype.command = function (signal) {
+    mediator.subscribe(signal);
 };
 
 (function () {
     var commander = new Commander(),
-        cmd = document.querySelector(".cmd");
+        cmd       = document.querySelector(".cmd");
 
     addHandler(cmd, "click", function (event) {
-        var btn    = getTarget(event),
-            cmdBar = btn.parentNode,
-            index  = -1, commands = ["fly", "stop", "destroy"];
+        var btn      = getTarget(event),
+            cmdBar   = btn.parentNode,
+            index    = -1,
+            commands = ["fly", "stop", "destroy"];
 
         if(btn.id == "createBtn") {
-            // 发送创造新飞船指令
-            commander.command({
-                command: "create"
-            });
-            console.log("create command sent");
-        } else {
-            // 根据index向Mediator发送不同指令
+            // 创造新飞船
+            for(var iter = 0; iter < 4; iter++) {
+                if(!mediator.ships[iter]) {
+                    ship = new Ship(iter+1);
+                    mediator.addShip(ship, iter);
+                    console.log("create new ship");
+                    break;
+                }
+            }
+        } else if (btn.tagName == "BUTTON") {
+            // 根据index向BUS发送不同指令
             [].forEach.call(cmdBar.querySelectorAll("button"), function (tempBtn, tempIndex) {
                 if(btn == tempBtn) {
                     index = tempIndex;
@@ -223,6 +226,8 @@ var Commander = function () {
                 command: commands[index]
             });
             console.log(commands[index] + " command sent from commander");
+        } else {
+            console.log("keep working...");
         }
     });
 })();
